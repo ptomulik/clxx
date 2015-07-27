626dd03d09903b650130419a0c2952b5664bde5d
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2015-07-27 03:42:09 +0200

    revision in program_lazy_generator.* and kernel_lazy_constructor.*

--- src/lib/clxx/cl/program_lazy_generator.cpp
+++ src/lib/clxx/cl/program_lazy_generator.cpp
@@ -10,53 +10,27 @@
 
 namespace clxx {
 /* ----------------------------------------------------------------------- */
+program_lazy_generator::
+program_lazy_generator()
+  : program_generator(),
+   _lazy_get_program(make_memoized_function(&clxx::program_generator::get_program, this))
+{ }
+/* ----------------------------------------------------------------------- */
 clxx::program program_lazy_generator::
 get_program(clxx::context const& context) const
-{
-  // To be thread-safe we have to protect mutable member(s)
-  // _programs_mutex is automatically released when lock goes out of scope
-  std::lock_guard<std::mutex> lock(this->_programs_mutex);
-
-  memoized_programs_t::const_iterator it{ this->_programs.find(context) }; 
-
-  if(it != _programs.cend())
-    return it->second;
-
-  return _programs[context] = this->program_generator::get_program(context);
-}
+{ return _lazy_get_program(context); }
 /* ----------------------------------------------------------------------- */
 clxx::program program_lazy_generator::
-get_memoized_program(clxx::context const& context) const
-{
-  // To be thread-safe we have to protect mutable member(s)
-  // _programs_mutex is automatically released when lock goes out of scope
-  std::lock_guard<std::mutex> lock(this->_programs_mutex);
-
-  memoized_programs_t::const_iterator it{ this->_programs.find(context) }; 
-
-  if(it == _programs.cend())
-    throw invalid_key_error("memoized program not found");
-
-  return it->second;
-}
+get_cached_program(clxx::context const& context) const
+{ return _lazy_get_program.get_cached_result(context); }
 /* ----------------------------------------------------------------------- */
 size_t program_lazy_generator::
-discard_memoized_program(clxx::context const& context) const
-{
-  // To be thread-safe we have to protect mutable member(s)
-  // _programs_mutex is automatically released when lock goes out of scope
-  std::lock_guard<std::mutex> lock(this->_programs_mutex);
-  return _programs.erase(context);
-}
+discard_cached_program(clxx::context const& context) const
+{ return _lazy_get_program.discard_cached_result(context); }
 /* ----------------------------------------------------------------------- */
 void program_lazy_generator::
-discard_memoized_programs() const
-{
-  // To be thread-safe we have to protect mutable member(s)
-  // _programs_mutex is automatically released when lock goes out of scope
-  std::lock_guard<std::mutex> lock(this->_programs_mutex);
-  _programs.clear();
-}
+discard_cached_programs() const
+{ _lazy_get_program.discard_cached_results(); }
 /* ----------------------------------------------------------------------- */
 } // end namespace clxx
 
