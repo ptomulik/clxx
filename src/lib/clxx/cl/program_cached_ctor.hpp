1a8aae3f9159718460d8ba9e26ba3b3393c23ebc
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2015-08-09 10:35:24 +0200

    backup commit 2015.08.09 10:35

--- src/lib/clxx/cl/program_cached_ctor.hpp
+++ src/lib/clxx/cl/program_cached_ctor.hpp
@@ -13,10 +13,8 @@
 #include <clxx/cl/program_with_source_ctor.hpp>
 #include <clxx/cl/context_fwd.hpp>
 #include <clxx/common/program_sources.hpp>
-#include <clxx/common/shared_ptr.hpp>
 #include <string>
 #include <vector>
-#include <locale>
 
 namespace clxx {
 /** // doc: program_cached_ctor {{{
@@ -25,58 +23,10 @@ namespace clxx {
 class program_cached_ctor
   : public program_with_source_ctor
 {
-public:
-  typedef wchar_t path_wchar_t;
-  typedef std::basic_string<path_wchar_t> path_wstring_t;
-  typedef std::vector<path_wstring_t> path_set_t;
 private:
-  static thread_local path_set_t _default_cache_path;
-  path_set_t _cache_path;
+  static thread_local std::vector<std::string> _default_cache_search_paths;
+  std::vector<std::string> _cache_search_paths;
 public:
-  /** // doc: append_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void append_default_cache_wpath(path_wstring_t const& wpath);
-  /** // doc: append_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void append_default_cache_path(std::string const& path,
-                                        std::string const& charset);
-  /** // doc: append_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void append_default_cache_path(std::string const& path,
-                                        std::locale const& locale = std::locale());
-  /** // doc: remove_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void remove_default_cache_wpath(path_wstring_t const& wpath);
-  /** // doc: remove_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void remove_default_cache_path(std::string const& path,
-                                        std::string const& charset);
-  /** // doc: remove_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void remove_default_cache_path(std::string const& path,
-                                        std::locale const& locale = std::locale());
-  /** // doc: get_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static path_wstring_t get_default_cache_wpath();
-  /** // doc: get_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static std::string get_default_cache_path(std::string const& charset);
-  /** // doc: get_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static std::string get_default_cache_path(std::locale const& locale = std::locale());
-  /** // doc: clear_default_cache_path() {{{
-   * \todo Write documentation
-   */ // }}}
-  static void clear_default_cache_path();
 public:
   /** // doc: operator() {{{
    * \todo Write documentation
--- src/test/clxx/cl/program_cached_ctor.t.h
+++ src/test/clxx/cl/program_cached_ctor.t.h
@@ -21,67 +21,13 @@ namespace clxx { class program_cached_ctor_test_suite; }
  */ // }}}
 class clxx::program_cached_ctor_test_suite : public CxxTest::TestSuite
 {
-#ifdef _WIN32
-#define PATHSEP L";"
-#else
-#define PATHSEP L":"
-#endif
 public:
-  /** // doc: test__clear_default_cache_path__1() {{{
+  /** // doc: test__foo() {{{
    * \todo Write documentation
    */ // }}}
-  void test__clear_default_cache_path__1( )
+  void test__foo( )
   {
-    program_cached_ctor::clear_default_cache_path();
-    TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"");
-  }
-  /** // doc: test__append_default_cache_wpath__0() {{{
-   * \todo Write documentation
-   */ // }}}
-  void test__append_default_cache_wpath__0( )
-  {
-    {
-      program_cached_ctor::clear_default_cache_path();
-      program_cached_ctor::append_default_cache_wpath(L"");
-      TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"")
-    }
-    {
-      program_cached_ctor::clear_default_cache_path();
-      program_cached_ctor::append_default_cache_wpath(PATHSEP);
-      TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"")
-    }
-    {
-      program_cached_ctor::clear_default_cache_path();
-      program_cached_ctor::append_default_cache_wpath(PATHSEP PATHSEP);
-      TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"")
-    }
-  }
-  /** // doc: test__append_default_cache_wpath__1() {{{
-   * \todo Write documentation
-   */ // }}}
-  void test__append_default_cache_wpath__1( )
-  {
-    program_cached_ctor::clear_default_cache_path();
-    program_cached_ctor::append_default_cache_wpath(L"foo1/bar1");
-    TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"foo1/bar1")
-  }
-  /** // doc: test__append_default_cache_wpath__2() {{{
-   * \todo Write documentation
-   */ // }}}
-  void test__append_default_cache_wpath__2( )
-  {
-    program_cached_ctor::clear_default_cache_path();
-    program_cached_ctor::append_default_cache_wpath(L"foo1/bar1" PATHSEP "foo2/bar2/geez2");
-    TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"foo1/bar1" PATHSEP "foo2/bar2/geez2")
-  }
-  /** // doc: test__append_default_cache_wpath__3() {{{
-   * \todo Write documentation
-   */ // }}}
-  void test__append_default_cache_wpath__3( )
-  {
-    program_cached_ctor::clear_default_cache_path();
-    program_cached_ctor::append_default_cache_wpath(L"foo1/bar1" PATHSEP PATHSEP "foo2/bar2/geez2");
-    TS_ASSERT_EQUALS(program_cached_ctor::get_default_cache_wpath(), L"foo1/bar1" PATHSEP "foo2/bar2/geez2")
+    TS_ASSERT(true);
   }
 };
 
