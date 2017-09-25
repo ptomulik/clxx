dd44a987a617e81678717e9121176456a6fbbdd1
Paweł Tomulik <ptomulik@meil.pw.edu.pl>
2017-09-25 19:20:09 +0200

    backup commit 2017.09.25 19:20

--- src/lib/clxx/README.rst
+++ src/lib/clxx/README.rst
@@ -2,5 +2,6 @@ TEMPORARY NAMES EXPLAINED
 -------------------------
 
 
-- ``b5d``   - backend, an intermediate API for accessing libOpenCL functionality,
+- ``b5d``   - "backend", an intermediate, functional API for accessing libOpenCL functionality,
+- ``b3c``   - "basic", basic object-oriented API for OpenCL stuff,
 - ``x10d``  - extend, extended object-oriented API for OpenCL applications.
---
+++ src/lib/clxx/b3c/command_queue_fwd.hpp
@@ -0,0 +1,21 @@
+// @COPYRIGHT@
+// Licensed under MIT license (LICENSE.txt)
+
+// clxx/b3c/command_queue_fwd.hpp
+
+/** // doc: clxx/b3c/command_queue_fwd.hpp {{{
+ * \file clxx/b3c/command_queue_fwd.hpp
+ * \brief Forward declaration of the \ref clxx::command_queue "command_queue" class
+ */ // }}}
+#ifndef CLXX_B3C_COMMAND_QUEUE_FWD_HPP_INCLUDED
+#define CLXX_B3C_COMMAND_QUEUE_FWD_HPP_INCLUDED
+
+#include <clxx/b5d/opencl.h>
+
+namespace clxx {
+class alignas(cl_command_queue) command_queue;
+} // end namespace clxx
+
+#endif /* CLXX_B3C_COMMAND_QUEUE_FWD_HPP_INCLUDED */
+// vim: set expandtab tabstop=2 shiftwidth=2:
+// vim: set foldmethod=marker foldcolumn=4:
